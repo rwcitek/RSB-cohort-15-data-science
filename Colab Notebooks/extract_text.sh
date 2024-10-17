c28bf808ddddec4ed2e4749cb8cb676b477cc05d
RSBalchII <robertbalchii@gmail.com>
2024-10-16 18:12:49 -0600

    wrote script to run extract text as json to .txt files in Colab Notebooks

--- Colab Notebooks/extract_text.sh
+++ Colab Notebooks/extract_text.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+## to use this script copy the code and paste it in the terminal where your ipynb files are. This will produce extrated text files for each notebook. These files can easily be uploaded to your notebookLM for sources .
 for notebook in *.ipynb; do
     jq -r '.cells[] | select(.cell_type=="markdown") | .source[]' "$notebook" > "${notebook%.ipynb}.txt"
 done
